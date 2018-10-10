279a5c15734aac008ea13f9e4f9101d0ee1e7884
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-10-10 11:58:44 +0200

    Lesweek 3 program

---
+++ lesweken/week3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // * Afdrukken van tekst in de console
+            Console.WriteLine("Hello there.");
+
+            /* Hallo
+            * Dit is een comment block
+            ! Thanks Better Comment
+            ! Deze app doet niet veel
+             */
+
+            // Rekenmachine
+            // * int c,d; kan ook als waarde later wordt toegekend
+            int a = 16;
+            int b = 6;
+
+            int result1 = optellen(a, b);
+            int result2 = deling(a, b);
+            int result3 = verhogen(a);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+        }
+
+        static int optellen(int a, int b) {
+            return a+b;
+        }
+
+        static int verhogen(int a) {
+            return a++;
+        }
+
+        static int deling(int a, int b) {
+            return a/b;
+        }
+    }
+}
