a7d6890d732229a73d45a598d78336b63858fee0
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-10-17 12:26:04 +0200

    opdracht 2 upload

--- opdrachten/opdracht2/Program.cs
+++ opdrachten/opdracht2/Program.cs
@@ -16,7 +16,25 @@ namespace opdracht2
             }
 
             Console.WriteLine(fac);
+            
+            
             // Fibonacci
+            int g0 = 0;
+            int g1 = 1;
+            int next = 0;
+
+            for(int i = 0; i < 15; i++) {
+                if(i == 0) {
+                    next = 0;
+                } else if(i == 1) {
+                    next = 1;
+                } else {
+                    next = g0 + g1;
+                    g0 = g1;
+                    g1 = next;
+                }
+                Console.WriteLine(next);
+            }
         }
     }
 }
