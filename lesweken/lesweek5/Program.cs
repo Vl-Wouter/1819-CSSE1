926f623088457b0d4c3f4171c76ba04de49164d9
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-10-24 10:49:17 +0200

    Week 5

---
+++ lesweken/lesweek5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesweek5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string text = "  Dit is een test.";
+            
+            for (int i = 0; i < text.Length; i++) 
+            {
+                Console.WriteLine(text.Substring(i, 1));
+            }
+
+            string[] woorden = text.Split();
+            foreach (var woord in woorden)
+            {
+                Console.WriteLine(woord);
+            }
+
+            Console.WriteLine(text.ToLower());
+            Console.WriteLine(text.ToUpper());
+            Console.WriteLine(text.Trim(' '));
+        }
+    }
+}
