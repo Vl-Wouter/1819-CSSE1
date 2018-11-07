8631323becc3046f72fa73dc3cc02e5bcd514365
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-11-07 09:33:24 +0100

    Refactor Opdracht

--- opdrachten/opdracht2/Program.cs
+++ opdrachten/opdracht2/Program.cs
@@ -6,34 +6,51 @@ namespace opdracht2
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            
+            // Alle tafels
+            program.AlleTafels();
+            
             // Tafel
             Console.WriteLine("Geef een getal in:");
             int tafel = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i < 10; i++) {
-                Console.WriteLine(i + " * " + tafel + " = " + (i*tafel));
-            }
+            program.Tafel(tafel);
             
             // Factulteit
             Console.WriteLine("Geef een getal in:");
             int getal = Convert.ToInt32(Console.ReadLine());
-            int fac = getal;
+            program.Faculteit(getal);
 
-            while(getal > 2) {
-                getal--;
-                fac *= getal;
-            }
-
-            Console.WriteLine(fac);
-            
+            // Faculteit 10
+            program.Faculteit(10);
             
             // Fibonacci
             Console.WriteLine("Geef een eindgetal in:");
             int eindfib = Convert.ToInt32(Console.ReadLine());
+            program.Fibonacci(eindfib);
+        }
+
+        void Tafel(int getal) {
+            for(int i = 0; i <= 10; i++) {
+                Console.WriteLine(i + " * " + getal + " = " + (i*getal));
+            }
+        }
+
+        void AlleTafels() {
+            for(int i = 1; i <= 10; i++) {
+                Console.WriteLine("Tafel van: " + i);
+                for(int j = 1; j <= 10; j++) {
+                    Console.WriteLine(i + " * " + j + " = " + i*j);
+                }
+            }
+        }
+
+        void Fibonacci(int getal) {
             int g0 = 0;
             int g1 = 1;
             int next = 0;
 
-            for(int i = 0; i < eindfib; i++) {
+            for(int i = 0; i < getal; i++) {
                 if(i == 0) {
                     next = 0;
                 } else if(i == 1) {
@@ -45,6 +62,17 @@ namespace opdracht2
                 }
                 Console.WriteLine(next);
             }
+        }
+
+        void Faculteit(int getal) {
+            int fac = getal;
+
+            while(getal > 2) {
+                getal--;
+                fac *= getal;
+            }
+
+            Console.WriteLine(fac);
         }
     }
 }
