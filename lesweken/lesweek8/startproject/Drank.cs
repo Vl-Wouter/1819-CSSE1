d5d4d1639cd0b2c541308b466f1e8900049be6ee
gdm-1819-woutvlae <woutvlae@student.arteveldehs.be>
2018-11-21 11:53:50 +0100

    Lesweek 8

---
+++ lesweken/lesweek8/startproject/Drank.cs
@@ -0,0 +1,14 @@
+namespace startproject
+  {
+  public class Drank  : Product
+  {
+    private int hoeveelheid;
+    private bool alcoholAanwezig;
+    
+    public override int BerekenKCal()
+      {
+          return 20;
+      }
+  }
+
+}
